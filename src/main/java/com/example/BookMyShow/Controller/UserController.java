dc3b1371b3a879d4d4386aa499338ba41e387cbb
JAYARANI MANIMARAN <jayarani.m2023aiml@sece.ac.in>
2025-12-18 14:56:50 +0530

    create and get user

---
+++ src/main/java/com/example/BookMyShow/Controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.BookMyShow.Controller;
+
+
+
+import com.example.BookMyShow.Entity.User;
+import com.example.BookMyShow.Service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+
+
+}
+
---
+++ src/main/java/com/example/BookMyShow/Service/UserService.java
@@ -0,0 +1,28 @@
+package com.example.BookMyShow.Service;
+
+
+import com.example.BookMyShow.Entity.User;
+import com.example.BookMyShow.Repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+
+}
