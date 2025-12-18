9bfdd83782f9754a0db63366898df74f71445559
JAYARANI MANIMARAN <jayarani.m2023aiml@sece.ac.in>
2025-12-18 15:10:07 +0530

    create and get movies

---
+++ src/main/java/com/example/BookMyShow/Controller/MovieController.java
@@ -0,0 +1,32 @@
+package com.example.BookMyShow.Controller;
+
+
+import com.example.BookMyShow.Entity.Movie;
+import com.example.BookMyShow.Service.MovieService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @PostMapping
+    public Movie createMovie(@RequestBody Movie movie) {
+        return movieService.createMovie(movie);
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return movieService.getAllMovies();
+    }
+
+
+}
+
---
+++ src/main/java/com/example/BookMyShow/Service/MovieService.java
@@ -0,0 +1,28 @@
+package com.example.BookMyShow.Service;
+
+import com.example.BookMyShow.Entity.Movie;
+import com.example.BookMyShow.Repository.MovieRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public Movie createMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+
+}
+
